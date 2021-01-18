0bf30399c5f193fcf4f41faee5a9664b75c0673f
Green <warren.green@bt.com>
2021-01-18 09:47:13 +0000

    initial project creation

---
+++ src/main/java/com/github/warrengreen/contrastapi/ContrastApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.warrengreen.contrastapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContrastApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContrastApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/warrengreen/contrastapi/ContrastApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.warrengreen.contrastapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContrastApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
