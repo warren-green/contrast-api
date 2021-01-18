2f082c59751e71b6b088f1366d3c38b4be5aa4ad
Warren Green <warren.green@bt.com>
2021-01-18 13:11:26 +0000

    add constructors for org

--- src/main/java/com/github/warrengreen/contrastapi/model/Organization.java
+++ src/main/java/com/github/warrengreen/contrastapi/model/Organization.java
@@ -12,13 +12,17 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Organization {
 
 	@Id
